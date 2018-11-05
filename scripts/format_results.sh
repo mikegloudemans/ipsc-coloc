65de371552e6465b718b9371797569c859529e35
mikegloudemans <mgloud@durga.stanford.edu>
2018-11-05 14:39:23 -0800

    First commit

---
+++ scripts/format_results.sh
@@ -0,0 +1,4 @@
+cat /users/mgloud/projects/brain_gwas/output/ipsc-tests/*/*clpp* | head -n 1 > /users/mgloud/projects/ipsc/output/full_ipsc_coloc_results.txt
+cat /users/mgloud/projects/brain_gwas/output/ipsc-tests/*/*clpp* | sort -k6,6gr | grep -v feature >> /users/mgloud/projects/ipsc/output/full_ipsc_coloc_results.txt
+
+cp -r /users/mgloud/projects/brain_gwas/output/ipsc-tests/*/plots/* /users/mgloud/projects/ipsc/output/plots
